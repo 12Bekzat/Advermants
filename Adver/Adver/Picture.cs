cdfbe6507022cf935cd619c8102447bbbc13d9ed
Toleutai Bekzat <toleutaib@gmail.com>
2019-12-02 19:34:43 +0600

    Сделал рекламу

---
+++ Adver/Adver/Picture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Adver
+{
+	public class Picture
+	{
+		public Guid Id { get; set; } = Guid.NewGuid();
+		public Guid GameId { get; set; }
+		public virtual Game Game { get; set; }
+		public string Value { get; set; }
+
+		public override string ToString()
+		{
+			return Value;
+		}
+	}
+}
